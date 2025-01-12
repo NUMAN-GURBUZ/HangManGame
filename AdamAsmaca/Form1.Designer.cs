64185380435c9b6731a6cff255ef3796d617a9b6
NUMAN GÜRBÜZ <146639528+NUMAN-GURBUZ@users.noreply.github.com>
2025-01-12 19:02:11 +0300

    HangManGame
    c# Devexpress simple Game

---
+++ AdamAsmaca/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace AdamAsmaca
+{
+    partial class KelımeGosterımı
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.KelimeGösterimi = new System.Windows.Forms.Label();
+            this.GırısButtonu = new System.Windows.Forms.Button();
+            this.KelimeGirisBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(765, 161);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(94, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(765, 161);
+            this.label2.TabIndex = 0;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // KelimeGösterimi
+            // 
+            this.KelimeGösterimi.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.KelimeGösterimi.Font = new System.Drawing.Font("Rockwell", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KelimeGösterimi.Location = new System.Drawing.Point(71, 9);
+            this.KelimeGösterimi.Name = "KelimeGösterimi";
+            this.KelimeGösterimi.Size = new System.Drawing.Size(850, 92);
+            this.KelimeGösterimi.TabIndex = 0;
+            this.KelimeGösterimi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // GırısButtonu
+            // 
+            this.GırısButtonu.BackColor = System.Drawing.Color.LightCyan;
+            this.GırısButtonu.Font = new System.Drawing.Font("Modern No. 20", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GırısButtonu.Location = new System.Drawing.Point(560, 433);
+            this.GırısButtonu.Name = "GırısButtonu";
+            this.GırısButtonu.Size = new System.Drawing.Size(226, 72);
+            this.GırısButtonu.TabIndex = 1;
+            this.GırısButtonu.Text = "GİRİŞ\r\n";
+            this.GırısButtonu.UseVisualStyleBackColor = false;
+            this.GırısButtonu.Click += new System.EventHandler(this.GırısButtonu_Click);
+            // 
+            // KelimeGirisBox
+            // 
+            this.KelimeGirisBox.BackColor = System.Drawing.Color.LightCyan;
+            this.KelimeGirisBox.Font = new System.Drawing.Font("Rockwell", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KelimeGirisBox.Location = new System.Drawing.Point(257, 436);
+            this.KelimeGirisBox.Multiline = true;
+            this.KelimeGirisBox.Name = "KelimeGirisBox";
+            this.KelimeGirisBox.Size = new System.Drawing.Size(150, 72);
+            this.KelimeGirisBox.TabIndex = 2;
+            this.KelimeGirisBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(71, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 296);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // KelımeGosterımı
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.Tan;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 546);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.KelimeGirisBox);
+            this.Controls.Add(this.GırısButtonu);
+            this.Controls.Add(this.KelimeGösterimi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "KelımeGosterımı";
+            this.Text = "Adam Asmaca Oyunu";
+            this.Load += new System.EventHandler(this.KelımeGosterımı_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label KelimeGösterimi;
+        private System.Windows.Forms.Button GırısButtonu;
+        private System.Windows.Forms.TextBox KelimeGirisBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AdamAsmaca/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdamAsmaca
+{
+    public partial class KelımeGosterımı : DevExpress.XtraEditors.XtraForm
+    {
+
+        List<string> words =new List<string> 
+        {
+    "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin",
+    "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur",
+    "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan",
+    "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Iğdır", "Isparta", "İstanbul",
+    "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kırıkkale", "Kırklareli", "Kırşehir",
+    "Kilis", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş",
+    "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas",
+    "Şanlıurfa", "Şırnak", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak"
+        }; //oyunda kullanılacak olan kelimeleri tanımladık
+        int DogruTahmin;//TAHMİN SAYISI İLE İLGİSİ TANIM YAPTIK
+        Random random;//KELİMELERİN RASTGELE EKRANA ÇIKMASI İÇİN TANIMLANDI
+        string SeciliKelime;//RANDOM OLARAK KULLANILAN KELİM BELİRLENMESİ   
+        char[] GorunenKelıme;//BİRER BİRER KELİME GİRMESİNİ SAĞLAR
+        
+
+        public KelımeGosterımı()
+        {
+            InitializeComponent();//FORMUN GÖRSEL TASARIMINA YARAR
+        }
+
+        private void KelımeGosterımı_Load(object sender, EventArgs e)
+        {
+            DogruTahmin = 0;//TAHMİN SAYISI 0 DAN BAŞLASIN 
+            random = new Random();//RANDOM TANIMLAMA
+            SeciliKelime = words[random.Next(words.Count)];//OYUNA RASTGELE ŞEHİR TANIMLAMASI 
+            GorunenKelıme = new string('_', SeciliKelime.Length).ToCharArray();//
+           string GırılenGorünenKelime = string.Join(" ", GorunenKelıme);//
+            KelimeGösterimi.Text = GırılenGorünenKelime;//EŞİTLİK DURUMUNDA CLASSLA ÇALIŞIR
+        }
+
+        private void GırısButtonu_Click(object sender, EventArgs e)
+        {
+            char Gırıs = KelimeGirisBox.Text.ToLower()[0];
+            bool BoxDogruTahmin = false;
+            for (int i = 0; i < SeciliKelime.Length; i++) //SEÇİLEN KELİMENİN GİRİLDİĞİNDE DEVAM ETMESİ SAĞLANIR
+            {
+                if (SeciliKelime[i]==Gırıs)//SEÇİLİ KELİME GİRİŞE EŞİTLENİR
+                {
+                    GorunenKelıme[i] = Gırıs;
+                    BoxDogruTahmin = true;//HARF DOĞRU İSE TEKRAR DÖNER
+                }
+            }
+            KelimeGösterimi.Text = string.Join(" ", GorunenKelıme);//DOĞRU KELİME YERİNİ ALIR
+            if(!BoxDogruTahmin)//DOĞRU OLMADIĞINDA ÇALIŞIR
+            {
+                UpdateAdamAsmacaResım();//HATADA ADIM ADIM RESİMLER İŞLENİR
+            }
+            if(!KelimeGösterimi.Text.Contains('_'))//DOĞRU OLDUĞUNDA BLOG ÇALIŞIR
+            {
+                MessageBox.Show("TEBRİKLER! KAZANDINIZ");//KAZANANI GÖSTERİR
+                Application.Restart();//OYUN TEKRARLANIR
+            }
+
+
+        }
+
+        private void UpdateAdamAsmacaResım()//BU CLASS'DA HATA OLDUKÇA ADIM İLERLEYEREK KALAN HAKKI GÖSTERİR
+        {
+            DogruTahmin++;//OYUN DEVAM EDER
+            switch(DogruTahmin)//TAHMİNİN YANLIŞ OLMASI DURUMUNDA CASE ADIM ADIM ÇALIŞIR
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources._2;//1.HATADA GÖRSEL ORTAYA ÇIKAR 
+                    break;//CASE YAPISINI DURDURUR
+                case 2:
+                    pictureBox1.Image = Properties.Resources._3;//2.HATADA GÖRSEL ORTAYA ÇIKAR 
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources._4;//3.HATADA GÖRSEL ORTAYA ÇIKAR 
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources._5;//4.HATADA GÖRSEL ORTAYA ÇIKAR 
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources._6;//5.HATADA GÖRSEL ORTAYA ÇIKAR 
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources._7;//6.HATADA GÖRSEL ORTAYA ÇIKAR 
+
+                    MessageBox.Show("KAYBETTİNİZ TEKRAR DENEYİN");//EKRANA YAZI ÇIKAR OYUN BİTTİ DİYE
+                    Application.Restart();//KAYBEDİNCE TEKRARDAN BAŞLAMASI İÇİN KULLANILDI
+                    break;
+
+                
+            }
+        }
+        
+    }
+}
